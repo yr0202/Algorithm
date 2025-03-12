64770fd7bb734519ca4b5489b13dff8186b21677
yr0202 <73926438+yr0202@users.noreply.github.com>
2025-03-12 10:01:21 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -3,10 +3,12 @@ check = [0]*divide_num
 
 for i in range(10):
     n = int(input())
-    check[n%divide_num]+=1
+    check[n%divide_num]=1
+    # check[n%divide_num] += 1
 
 answer = 0
 for i in range(divide_num):
-    if check[i] != 0: answer+=1
+    answer += check[i]
+    # if check[i] != 0: answer+=1
 
 print(answer)
